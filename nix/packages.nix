827914493459d548d75cae9219fdb0d26780df64
Jinnah Ali-Clarke <jinnah@noredink.com>
2023-12-12 18:23:31 -0500

    update to nixpkgs 23.05, use niv from nixpkgs

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,9 +1,6 @@
 let
   sources = import ./sources.nix { };
-  nivOverlay = _: pkgs: {
-    niv = (import sources.niv { }).niv; # use the sources :)
-  };
   servant-oidcOverlay = _: pkgs: {
     servant-oidc = pkgs.haskellPackages.callCabal2nix "servant-oidc" ../. { };
   };
-in import (sources.nixpkgs) { overlays = [ nivOverlay servant-oidcOverlay ]; }
+in import (sources.nixpkgs) { overlays = [ servant-oidcOverlay ]; }
